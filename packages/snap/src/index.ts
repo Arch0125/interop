34f2b3a59a302778ebf2da1e0dd3ef6211818b56
Archisman Das <72722967+Arch0125@users.noreply.github.com>
2023-05-27 06:42:55 +0530

    add stackup sdk

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,32 +1,39 @@
 import { OnRpcRequestHandler } from '@metamask/snaps-types';
-import { panel, text } from '@metamask/snaps-ui';
+import { divider, heading, panel, text } from '@metamask/snaps-ui';
+import { SLIP10Node } from '@metamask/key-tree';
+import {ethers} from 'ethers';
+import { getAbstractAccount } from './utils/initaccount';
+import { SimpleAccountAPI } from '@account-abstraction/sdk';
+import { Presets } from "userop";
 
-/**
- * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
- *
- * @param args - The request handler args as object.
- * @param args.origin - The origin of the request, e.g., the website that
- * invoked the snap.
- * @param args.request - A validated JSON-RPC request object.
- * @returns The result of `snap_dialog`.
- * @throws If the request method is not valid for this snap.
- */
-export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
+
+export const onRpcRequest: OnRpcRequestHandler = async({ origin, request }) => {
   switch (request.method) {
-    case 'hello':
+    case 'aainit':{
+      const pvtkey = await await snap.request({
+        method: 'snap_getEntropy',
+        params: {
+          version: 1,
+          salt: 'foo',
+        },
+      });
+
+      const aa = await getAbstractAccount();
+      const address = aa.getSender()
+
       return snap.request({
         method: 'snap_dialog',
         params: {
           type: 'confirmation',
           content: panel([
-            text(`Hello, **${origin}**!`),
-            text('This custom confirmation is just for display purposes.'),
-            text(
-              'But you can edit the snap source code to make it do something, if you want to!',
-            ),
+            heading('Connection request'),
+            divider(),
+            text(`Do you want to connect **${origin}**! with`),
+            text('The Smart Account :'),
+            text(`**${address}**`),
           ]),
         },
-      });
+      });}
     default:
       throw new Error('Method not found.');
   }
---
+++ packages/snap/src/utils/initaccount.ts
@@ -0,0 +1,32 @@
+import { ethers } from 'ethers';
+import { SimpleAccountAPI } from '@account-abstraction/sdk';
+import { Presets } from 'userop';
+
+const entryPoint = '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789';
+const factoryAddress = '0x9406Cc6185a346906296840746125a0E44976454';
+const paymasterAPI =
+  'https://api.stackup.sh/v1/paymaster/09b0b15c992db3bc056c367840e10df3e816ea4a80a6a144d06d4e3f6a6547d1';
+const rpcUrl =
+  'https://api.stackup.sh/v1/node/09b0b15c992db3bc056c367840e10df3e816ea4a80a6a144d06d4e3f6a6547d1';
+
+export const getAbstractAccount = async () => {
+  const pvtkey = await await snap.request({
+    method: 'snap_getEntropy',
+    params: {
+      version: 1,
+      salt: 'foo',
+    },
+  });
+
+  const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+  const wallet = new ethers.Wallet(pvtkey, provider);
+
+  const simpleAccount = await Presets.Builder.SimpleAccount.init(
+    new ethers.Wallet(pvtkey),
+    rpcUrl,
+    entryPoint,
+    factoryAddress,
+  );
+
+  return simpleAccount;
+};
